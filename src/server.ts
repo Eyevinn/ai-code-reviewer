f6459b27a28150f284296d654a8df2f859e06d77
Jonas Birmé <jonas.birme@eyevinn.se>
2024-12-12 11:14:39 +0100

    fix: frontend was never served by service

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
+import fastifyStatic from '@fastify/static';
 import api from './api';
+import path from 'path';
 
-const server = api({ title: '@eyevinn/typescript-nodejs' });
+const server = api({ title: 'AI Code Reviewer' });
+server.register(fastifyStatic, {
+  root: path.join(__dirname, '../out'),
+  prefix: '/'
+});
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 8000;
 
